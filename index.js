6b8cd1a75e0fc2fc62cb327ef520b3976f212c22
Jesse Lee <jesselee613@gmail.com>
2020-04-17 20:39:24 -0700

    Add min requirements and min test script

--- index.js
+++ index.js
@@ -2,11 +2,38 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 /**
- * Respond with hello worker text
+ * Respond a random variant from base url
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
+  const url = "https://cfw-takehome.developers.workers.dev/api/variants";
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
   })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let variants = data.variants;   // hold url variants
+      let randUrl = getRandom(variants)   // get random url
+      // then fetch the random variant
+      const getVariant = fetch(randUrl).then((response) => {return response;});
+      return getVariant;
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+
+    console.log("Response:", response);
+    return response;
+  // return new Response(response, {
+  //   headers: { 'content-type': 'text/plain' },
+  // })
+}
+
+function getRandom(array) {
+  return array[Math.floor(Math.random() * Math.floor(array.length) % array.length)];
 }
